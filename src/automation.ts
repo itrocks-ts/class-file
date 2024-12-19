5d308f0ac1387597c8eced40403c630b667cf88b
Baptiste Pillot <baptiste@pillot.fr>
2024-12-19 09:15:50 +0100

    Fix automation for imports starting with './' or '../'

--- src/automation.ts
+++ src/automation.ts
@@ -14,7 +14,12 @@ Module.prototype.require = function(file: string)
 	const module = superRequire.call(this, ...arguments)
 	for (const object of Object.values(module)) {
 		if (!isAnyType(object)) continue
-		normalizedFile ??= normalize(require.resolve(file))
+		if (!normalizedFile) {
+			if (file[0] === '.') {
+				file = this.path + ((this.path[this.path.length - 1] === '/') ? '' : '/') + file
+			}
+			normalizedFile = normalize(require.resolve(file))
+		}
 		File(normalizedFile)(object)
 	}
 	return module
