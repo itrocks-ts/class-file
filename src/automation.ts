720aa1169b3bb3326cc477bc6cafeb3548b62214
Baptiste Pillot <baptiste@pillot.fr>
2025-07-21 13:35:07 +0200

    Skip init on circular import with incomplete exports

--- src/automation.ts
+++ src/automation.ts
@@ -3,25 +3,27 @@ import { normalize }           from 'node:path'
 import { File, fileOf }        from './class-file'
 
 const already = new Set<string>()
+const recurse = new Set<string>()
 
 const Module = require('module')
 const superRequire: (...args: any) => typeof Module = Module.prototype.require
 
 Module.prototype.require = function(file: string)
 {
-	const module = superRequire.call(this, ...arguments)
-
 	if (file[0] === '.') {
 		file = this.path + ((this.path[this.path.length - 1] === '/') ? '' : '/') + file
 	}
 	file = normalize(require.resolve(file))
-	if (file[0] !== '/') {
+
+	const recurses = recurse.has(file)
+	recurse.add(file)
+	const module = superRequire.call(this, ...arguments)
+	recurse.delete(file)
+
+	if (recurses || (file[0] !== '/') || already.has(file)) {
 		return module
 	}
 
-	if (already.has(file)) {
-		return module
-	}
 	already.add(file)
 
 	for (const object of Object.values(module)) {
